a83f39e1baea3e271e6ffa0db3af9f345bd7786a
tta36emulator <lana_spsy@mail.ru>
2014-12-24 15:30:21 +0500

    version 0.001a
    first steps

---
+++ editor/CodeEditor.js
@@ -0,0 +1,234 @@
+var CodeEditor = function () {
+	var self = this;
+	var editStrings  = [];
+	var LIMIT = 80;
+	var Tags = {
+		writeTag: "<editable>"
+	}
+
+	var currentLineNum = 0;
+	var lines = 0;
+	var lineArray = [];
+
+	var dangerWords = [
+        'eval', '.call', 'call(', 'apply', 'bind',
+        'prototype',
+        'setTimeout', 'setInterval',
+        'requestAnimationFrame', 'mozRequestAnimationFrame',
+        'webkitRequestAnimationFrame', 'setImmediate',
+        'prompt', 'confirm',
+        'debugger',
+        'delete',
+        'atob(','btoa(',
+        'Function(',
+        'constructor',
+        'window',
+        'document',
+        'self.', 'self[', 'top.', 'top[', 'frames',
+        'parent', 'content',
+        'validate', 'onExit', 'objective',
+        'this['
+    ];
+
+    var warnings = [];
+
+	var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+					lineNumbers: true,
+					matchBrackets: true,
+					mode: 'javascript',
+					theme: "vibrant-ink",
+					indentUnit: 4,
+					lineWrapping:false,
+					dragDrop: false,
+					smartIndent: false,
+					disableSpellcheck: false
+				});   
+				editor.setSize(700, 603); 
+
+
+
+		self.preprocess = function(code){
+			 var lines = code.split("\n");
+			 var resultCode = "";
+			 editStrings.length = 0;
+			 warnings.length = 0;
+
+			 for(var i = 0; i < lines.length; i++){ 
+				if(lines[i].indexOf(Tags.writeTag) > -1){
+					lines[i] = lines[i].replace("<editable>","");
+					   editStrings.push(i);
+				}
+
+				resultCode += lines[i] + "\n";
+			 }
+
+			return resultCode;
+		};
+
+		var isReadOnly = function(num){
+			if(editStrings.indexOf(num) === -1) return true;
+			return false;
+		};
+
+		var shiftElements = function(arr,bound,n){
+	       return arr.map(function(num) {
+		        if (num > bound) {return num + n;} return num;
+	        });
+   		};
+
+   		var getEndWritableBlock = function(begin){
+   			var a = begin;
+			while(editStrings.indexOf(a) !== -1)
+				a++;
+			return a;
+   		};
+
+   		var removeElement = function(arr, elem) {
+   			for(var i = arr.length; i--;) {
+		        if(arr[i] === elem) {
+		            arr.splice(i, 1);
+		        }
+		    }
+   		};
+
+   		var inEditableArea = function(c) {
+            var lineNum = c.to.line;
+            if (editStrings.indexOf(lineNum) !== -1 && editStrings.indexOf(c.from.line) !== -1)          	
+                return true;
+            
+			return false;
+        };
+
+		var process = function(me, change){
+			if(!inEditableArea(change)){
+				change.cancel();
+				return;
+			}
+
+			var newLines = change.text.length - (change.to.line - change.from.line + 1);
+			var currentLine = change.to.line;
+
+			if(isReadOnly(currentLine)){
+	        	change.cancel();
+	        	return;
+	        }
+
+				//cut off 80 chars
+	        var textlen = me.getLine(currentLine).length;
+	        if (textlen + change.text[0].length > LIMIT) {
+	             var allowedLength = Math.max(LIMIT - textlen, 0);
+	                change.text[0] = change.text[0].substr(0, allowedLength);
+	        }
+
+			if(newLines > 0){
+				//onNewLines
+					var bound = getEndWritableBlock(currentLine);
+					editStrings = shiftElements(editStrings, bound, newLines);
+					for (var i = bound; i < bound + newLines; i++) {
+	            			editStrings.push(i);
+	        		}
+
+	        		editStrings.sort();
+			}
+			else if(change.to.line < change.from.line || change.to.line - change.from.line + 1 > change.text.length){
+				//onDeleteLines
+					var count = change.to.line - change.from.line - change.text.length + 1;
+					var bound = getEndWritableBlock(currentLine);
+
+					var begin = bound - 1;
+					var end   = bound - (count + 1);
+
+					if(isReadOnly(begin-1))
+					{
+						change.cancel();
+	        			return;
+					}
+
+					for (var i = begin; i > end; i--) {
+            			    removeElement(editStrings,i);
+       				}
+
+       				editStrings = shiftElements(editStrings, bound, -count);
+					editStrings.sort();
+			}
+		};
+
+		self.loadCode = function(code){	
+			//editor.off('beforeChange', process);
+				var ccode = self.preprocess(code);
+				editor.setValue(ccode);
+				editor.addLineClass(currentLineNum, "wrap", "currentLine"); 
+				lineArray = ccode.split("\n");
+				lines = lineArray.length;
+			//editor.on('beforeChange', process);
+		};
+
+		self.step = function(){
+			if(currentLineNum + 1 > lines)
+				return "";
+
+			var s = lineArray[currentLineNum];
+
+			editor.removeLineClass(currentLineNum, "wrap", "currentLine");
+			editor.addLineClass(currentLineNum, "wrap", "simpleLine");
+			  currentLineNum++;
+			editor.removeLineClass(currentLineNum, "wrap", "simpleLine");
+			editor.addLineClass(currentLineNum, "wrap", "currentLine");		
+			return s;
+		};
+
+		self.reset = function(){
+			editor.removeLineClass(currentLineNum, "wrap", "currentLine");
+			editor.addLineClass(currentLineNum, "wrap", "simpleLine");
+			  currentLineNum=0;
+			editor.removeLineClass(currentLineNum, "wrap", "simpleLine");
+			editor.addLineClass(currentLineNum, "wrap", "currentLine");		
+		};
+
+		self.loadState = function(code,css){	
+			editor.off('beforeChange', process);
+				var ccode = self.preprocess(code);
+
+				css = css.split(',');
+				for(var i = 0; i < css.length; i++)
+					editStrings.push(parseInt(css[i]));
+
+				editor.setValue(ccode);
+				var lineArray = ccode.split("\n");
+				for(var i = 0; i < lineArray.length; i++){
+					if(isReadOnly(i)){
+						editor.addLineClass(i, "wrap", "readOnly"); 
+					}
+				};
+			editor.on('beforeChange', process);
+		};
+
+		self.setValue = function(value){
+			editor.setValue(value);
+			editor.refresh();
+		};
+
+		self.getStrings = function(){
+			return editStrings;
+		};
+
+		self.getCode = function(){
+			return editor.getValue();
+		};
+
+		self.refresh = function(){
+			return editor.refresh();
+		};
+		self.focus = function(){
+			editor.focus();
+		};
+		editor.on("focus", function(instance) {
+			/*game.currentfocus = "editor";
+			var editorPane = document.getElementById('codePane');
+			editorPane.style.border = '2px solid #ffaaaa';*/
+        });
+		editor.on("blur", function(instance) {
+			var editorPane = document.getElementById('codePane');
+			editorPane.style.border = '2px solid #ccc';
+        });
+}
---
+++ scripts/compiler.js
@@ -0,0 +1,308 @@
+var Compiler = function(){
+
+	//#############################-Xcanvas-###########################################
+		var Xcanvas = function(ctx,cvs){
+			var _xcanvas = this;
+			var part = null;
+			var context = ctx;
+			var canvas = cvs;
+
+			_xcanvas.drawText = function(font, text, x, y, color){
+				context.font       = font;
+				context.fillStyle  = color;
+				context.fillText(text, x, y);
+			};
+
+			_xcanvas.clearColor = function(color){
+				context.fillStyle = color;
+				context.clearRect(0, 0, canvas.width, canvas.height);
+				context.fillRect(0, 0, canvas.width, canvas.height);
+			};
+
+			_xcanvas.clearCanvas = function(color,data){
+				context.fillStyle = color;
+
+				if(data !== undefined && data !== null)
+					context.putImageData(data,0,0);
+				else{
+					context.clearRect(0, 0, canvas.width, canvas.height);
+					context.fillRect(0, 0, canvas.width, canvas.height);
+				}
+			};
+
+			_xcanvas.clear = function(){
+				context.clearRect(0, 0, canvas.width, canvas.height);
+			}
+
+			_xcanvas.clearRect = function(rect){
+				context.clearRect(rect.x, rect.y, rect.w, rect.h);
+			}
+
+			_xcanvas.drawRect = function(rect,color,width){
+				context.beginPath();
+				context.rect(rect.x, rect.y, rect.w, rect.h);
+				context.lineWidth   = width;
+				context.strokeStyle = color;
+				context.stroke();
+			};
+
+			_xcanvas.fillRect = function(rect,color){
+				context.beginPath();
+				context.rect(rect.x, rect.y, rect.w, rect.h);
+				context.fillStyle   = color;
+				context.fill();
+				context.lineWidth   = 1;
+				context.strokeStyle = '#000';
+				context.stroke();
+			};
+
+			_xcanvas.drawPoint = function(x, y, size, color){
+				ctx.fillStyle = color;
+				ctx.beginPath();
+				ctx.arc(x, y, size, 0, 2*Math.PI, true);
+				ctx.fill();
+				ctx.closePath();
+			};
+
+			_xcanvas.imageSmoothingEnabled = function (state) {
+				context.mozImageSmoothingEnabled 	= state;
+				context.oImageSmoothingEnabled 		= state;
+				context.webkitImageSmoothingEnabled = state;
+				context.imageSmoothingEnabled 		= state;
+			};
+		}
+	//#############################-ENDXcanvas-########################################
+
+	_compiler = this;
+	var codeEditor  = new CodeEditor();
+	var screen  = document.getElementById('lay1');
+	var bg_context = screen.getContext('2d');
+	var xcanvas = new Xcanvas(bg_context, screen);
+
+	var dstTable  = document.getElementById('dstTable');
+	var srcTable  = document.getElementById('srcTable');
+	var currentCommand = document.getElementById('currentCommand');
+
+	xcanvas.clearCanvas('#ccf');
+	xcanvas.drawText('Console 12pt', 'SCREEN', 5, 10, '#000');
+
+	codeEditor.refresh();
+	codeEditor.loadCode("#10 -> ADD.A\n#10 -> ADD.B\nADD -> R1\n#10 -> ADD.A\n#10 -> ADD.B\nADD -> R1\n#10 -> ADD.A\n#10 -> ADD.B\nADD -> R1\n#10 -> ADD.A\n#10 -> ADD.B\nADD -> R1\n#10 -> ADD.A\n#10 -> ADD.B\nADD -> R1");
+
+	var zeroFill  = function( number, width )
+	{
+		width -= number.toString().length;
+		if ( width > 0 ) return new Array(width + (/\./.test(number) ? 2 : 1) ).join( '0' ) + number;
+		return number;
+	};
+
+	var toDec = function(hexNumber) {
+        return parseInt(hexNumber,16);
+	};
+
+	var toHex = function(number) {
+		number = number.toString(16).toUpperCase();
+	    return "0x" + zeroFill(number, 4);
+	};
+
+	var destroyChildren = function(node)
+	{
+		while (node.firstChild)
+			node.removeChild(node.firstChild);
+	};
+
+	var createHeaders = function(){
+		var row = document.createElement('tr');
+		var reg = document.createElement('td');
+		reg.className = 'regCol';
+		var val = document.createElement('td');
+		val.className = 'regCol';
+		reg.innerHTML = 'REG(dst)';
+		val.innerHTML = 'value';
+
+		var addr = document.createElement('td');
+		addr.innerHTML = 'Address';
+		addr.className = 'regCol';
+		row.appendChild(addr);
+		row.appendChild(reg);
+		row.appendChild(val);
+		dstTable.appendChild(row);
+
+		row = document.createElement('tr');
+		reg = document.createElement('td');
+		reg.className = 'regCol';
+		val = document.createElement('td');
+		val.className = 'regCol';
+		reg.innerHTML = 'REG(src)';		
+		val.innerHTML = 'value';
+
+		var addr = document.createElement('td');
+		addr.innerHTML = 'Address';
+		addr.className = 'regCol';
+
+		row.appendChild(addr);
+		row.appendChild(reg);
+		row.appendChild(val);
+		srcTable.appendChild(row);
+	}
+
+	var Core = function(){
+		var _core 		= this;
+		var sourses 	= {};
+		var destination = {};
+
+		sourses.R0  	= 0;
+		sourses.R1  	= 0;
+		sourses.R2  	= 0;
+		sourses.R3  	= 0;
+		sourses.R4  	= 0;
+		sourses.R5  	= 0;
+		sourses.R6  	= 0;
+		sourses.R7  	= 0;
+		sourses.R8  	= 0;
+		sourses.R9  	= 0;
+		sourses.R10 	= 0;
+		sourses.R11 	= 0;
+		sourses.R12 	= 0;
+		sourses.R13 	= 0;
+		sourses.R14 	= 0;
+		sourses.IP 		= 0;
+		sourses.LINK  	= 0;
+		sourses.ADD 	= 0;
+		sourses.ADDC 	= 0;
+		sourses.SUB 	= 0;
+		sourses.XOR 	= 0;
+		sourses.AND 	= 0;
+		sourses.STATE 	= 0;
+		sourses.PIN 	= 0;
+		sourses.CONST10 = 0;
+		sourses.CONST16 = 0;
+
+		destination.R0  	= 0;
+		destination.R1  	= 0;
+		destination.R2  	= 0;
+		destination.R3 	 	= 0;
+		destination.R4  	= 0;
+		destination.R5  	= 0;
+		destination.R6  	= 0;
+		destination.R7  	= 0;
+		destination.R8  	= 0;
+		destination.R9  	= 0;
+		destination.R10 	= 0;
+		destination.R11 	= 0;
+		destination.R12 	= 0;
+		destination.R13	 	= 0;
+		destination.R14 	= 0;
+		destination.IP  	= 0;
+		destination.ADDR 	= 0;
+		destination.DATA 	= 0;
+		destination["ADD.A"] 	= 10;
+		destination["ADD.B"] 	= 5;
+		destination["AND.A"] 	= 0;
+		destination["AND.B"] 	= 0;
+		destination["SUB.A"]	= 0;
+		destination["SUB.B"] 	= 0;
+		destination.SH 			= 0;
+		destination["ADDC.A"] 	= 0;
+		destination["ADDC.B"] 	= 0;
+		destination["XOR.A"] 	= 0;
+		destination["XOR.B"] 	= 0;
+		destination.STATE 		= 0;
+		destination.POUT 		= 0;
+		destination.NULL 		= 0;
+
+		_core.drawRegisters = function(){
+			destroyChildren(srcTable);
+			destroyChildren(dstTable);
+
+			var c = 0;
+
+			createHeaders();
+
+			for(var p in destination){
+				var row = document.createElement('tr');
+				row.setAttribute("id", "dst" + c);
+
+				var addr = document.createElement('td');
+				addr.innerHTML = toHex(c);
+
+				var reg = document.createElement('td');
+				reg.className = 'regCol';
+
+				var val = document.createElement('td');
+				reg.innerHTML = p;		
+				val.innerHTML = toHex(destination[p]);
+				row.appendChild(addr);
+				row.appendChild(reg);
+				row.appendChild(val);
+				dstTable.appendChild(row);
+				c++;
+			}
+
+			c = 0;
+			for(var p in sourses){
+				var addr = document.createElement('td');
+				addr.innerHTML = toHex(c);
+
+				var row = document.createElement('tr');
+				row.setAttribute("id", "src" + c);
+
+				var reg = document.createElement('td');
+				reg.className = 'regCol';
+
+				var val = document.createElement('td');
+				reg.innerHTML = p;		
+				val.innerHTML = toHex(sourses[p]);
+				row.appendChild(addr);
+				row.appendChild(reg);
+				row.appendChild(val);
+				srcTable.appendChild(row);
+				c++;
+			}
+		};
+
+		_core.selectRow = function(prefix, num){
+			var id = prefix + num;
+			var row = document.getElementById(id);
+			row.style.backgroundColor = "#cfc";
+		};
+
+		_core.update = function(){
+			sourses.ADD  = destination["ADD.A"]  + destination["ADD.B"];
+			sourses.ADDC = destination["ADDC.A"] + destination["ADDC.B"];
+			sourses.SUB  = destination["SUB.B"]  - destination["SUB.A"];
+			sourses.XOR  = destination["XOR.B"]  ^ destination["XOR.A"];
+			sourses.AND  = destination["AND.B"]  & destination["AND.A"];
+		};
+
+		_core.refresh = function(){
+			core.update();
+			core.drawRegisters();
+		};
+
+		_core.step = function(){
+			var s = codeEditor.step();
+			currentCommand.innerHTML = s;
+		};
+
+		_core.reset = function(){
+			for(var p in destination){
+				destination[p] = 0;
+			}
+
+			for(var p in sourses){
+				sourses[p] = 0;
+			}
+			codeEditor.reset();
+		};
+	};
+
+	_compiler.getCore = function(){
+		return core;
+	}
+
+	var core = new Core();	
+	core.refresh();
+	core.selectRow('src', 5);
+	core.selectRow('dst', 7);
+};
